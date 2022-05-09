7dee982146ef14a79fd1e318c6b7018effd12ded
devinazhifa <devinazhifa@gmail.com>
2022-05-09 16:46:55 +0700

    first commit

---
+++ src/store/user.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+import jwtDecode from "jwt-decode";
+
+let initialState = null // let karena bisa diubah
+
+try {
+    const token = localStorage.getItem('minishopAccessToken')
+    const userData = jwtDecode(token)
+    initialState = userData
+} catch{}
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        data: null
+    },
+    reducers: {
+        addUser: (state, action) => {
+            state.data = {...action.payload.userData}
+        },
+        removeUser: (state) => {
+            state.data = null
+        }
+    },
+})
+
+export default userSlice
